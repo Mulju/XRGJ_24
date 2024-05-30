05ec804732abaea0b33d86850e161c3a5e7078f1
Mulju <samuli.matias@hotmail.com>
2024-05-30 17:12:48 +0300

    wip arrow spawner

---
+++ Assets/Scripts/ArrowSpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject arrow;
+
+    public void SpawnArrow()
+    {
+
+    }
+}
