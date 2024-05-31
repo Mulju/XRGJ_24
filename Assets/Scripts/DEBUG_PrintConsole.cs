65c34c3c529d6e742474ec7a1dae9ee82fcdf880
Mulju <samuli.matias@hotmail.com>
2024-05-31 13:34:25 +0300

    Feetbow and other stuff

---
+++ Assets/Scripts/DEBUG_PrintConsole.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUG_PrintConsole : MonoBehaviour
+{
+    public void PrintConsole(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
